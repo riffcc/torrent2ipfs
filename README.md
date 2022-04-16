bfc757012dddeb8fc412d9bc0c67d0b79130c81a
Benjamin Arntzen <zorlin@gmail.com>
2022-04-16 00:38:04 +0000

    Mostly finished + README

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # torrent2ipfs
 BitTorrent to IPFS bridge
 
-Requires IPFS, rTorrent, Python3, xmlrpclib
+Requires IPFS, Python3, qBittorrent.
 
-test.torrent = ubuntu-20.04.4-live-server-amd64.iso.torrent
+torrent2ipfs will add all torrents from ingest/ to qBittorrent with a special tag, then monitor qBittorrent until it no longer has any torrents to download. It will create IPFS hashes for each torrent you added, storing them as a JSON file. It's smart enough to skip creating IPFS hashes for torrents it has already processed before.
+
+test.torrent is included for testing, which is actually ubuntu-20.04.4-live-server-amd64.iso.torrent
+
+## Setup
+You'll need to install some python libraries before running this.
+
+```
+pip3 install python-dotenv
+pip3 install qbittorrent-api
+pip3 install ipfshttpclient
+pip3 install humanfriendly
+```
+
+## Usage
+Place torrents to "convert" into ingest/, then run `python3 main.py`.
--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 # Load various libraries
 from dotenv import load_dotenv
 from qbittorrentapi import Client
+from humanfriendly import format_timespan
 
 import os
 import json
-import ipfsApi
+import ipfshttpclient
+
+print("torrent2ipfs starting... let's get interplanetary.")
 
 # Load .env
 load_dotenv()
@@ -20,33 +23,70 @@ label = os.getenv('label')
 # Load existing database of pairs
 if(os.path.isfile("pairs.json")):
     pairs = json.load(open("pairs.json"))
+else:
+    pairs = {}
 
+print("[Stage 1] Setting up connections.")
+print("Connecting to qBittorrent...")
 # Spawn a qbt_client object to interact with
 qbt_client = Client(host=url + ":" + port, username=username, password=password)
 
+print("Connecting to IPFS daemon...")
 # Spawn an IPFS client object to interact with
-api = ipfsApi.Client('127.0.0.1', 5001)
+ipfs_client = ipfshttpclient.connect()
 
+print("[Stage 2] Importing torrents from ingest/ folder.")
 # Get a list of torrents in the ingest folder, then load them into qBT
 for torrent in os.scandir('ingest/'):
     if torrent.is_file():
-        print(torrent.name)
+        print("Adding torrent " + torrent.name)
         qbt_client.torrents_add(torrent_files=open('ingest/' + torrent.name, "rb"), tags=label)
 
-#pairs = {"b44a0e20fa5b7cecb77156333b4268dfd7c30afb": "bar", "0cb7144515936e81f9d21d86af30f0627e02818a": "foo"}
-pairs = {"foo": "bar", "bar": "foo"}
+finished = False
 
-# For each completed torrent
-torrents_info = qbt_client.torrents_info(status_filter="completed", tag=label)
-for completed_torrent in torrents_info:
-    print(completed_torrent["hash"])
-    # Check if the info_hash is in our list of pairs
-    if(completed_torrent["hash"] in pairs):
-        print(pairs[completed_torrent["hash"]])
-    # If it is not...
+while finished == False:
+    print("[Stage 3A] Checking downloads in progress...")
+    # Grab a list of downloading torrents from qBittorrent
+    torrents_info = qbt_client.torrents_info(status_filter="downloading", tag=label)
+
+    print("Currently downloading " + str(len(torrents_info)) + " torrents.")
+
+    if len(torrents_info) != 0:
+        for torrent in torrents_info:
+            print(torrent["hash"] + ": estimated time remaining: " + format_timespan(torrent["eta"]))
     else:
-        res = api.add(completed_torrent["content_path"])
-        print(res)
+        finished = True
+
+    print("[Stage 3B] Adding completed torrents to IPFS.")
+
+    # Grab a list of completed/seeding torrents from qBittorrent
+    torrents_info = qbt_client.torrents_info(status_filter="completed", tag=label)
+    if len(torrents_info) == 0:
+        print("No completed torrents found yet.")
+    # For each completed torrent...
+    for completed_torrent in torrents_info:
+        # Check if the info_hash is in our list of pairs
+        if(completed_torrent["hash"] in pairs):
+            #print("Found IPFS hash for " + completed_torrent["hash"] + ":")
+            #print("ipfs://" + pairs[completed_torrent["hash"]])
+            pass
+        # If it is not...
+        else:
+            print("Torrent not in IPFS yet: " + completed_torrent["hash"])
+	    # Add it to IPFS
+            print("Adding to IPFS...")
+            if os.path.isdir(completed_torrent["content_path"]):
+                res = ipfs_client.add(completed_torrent["content_path"], recursive=True)
+                # and store the IPFS hash in our array.
+                print("New IPFS hash: " + res[0]["Hash"])
+                pairs[completed_torrent["hash"]] = res[0]["Hash"]
+            else:
+                res = ipfs_client.add(completed_torrent["content_path"])
+                print("New IPFS hash: " + res["Hash"])
+                pairs[completed_torrent["hash"]] = res["Hash"]
+
+print("Finished.")
+print("Writing pairs.json with all discovered hashes.")
 
 # Write database of pairs
 json.dump(pairs, open("pairs.json", 'w'))
